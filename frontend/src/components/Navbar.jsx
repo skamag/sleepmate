12b907c84eb3394c2f2a992566a0b0e9d825e446
unknown <magneskaro@gmail.com>
2025-01-22 12:35:12 +0100

    added navbar css

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 import { useCart } from "../context/CartContext"
-// import "./Navbar.css"
+import "./Navbar.css"
 
 function Navbar({
   data,
