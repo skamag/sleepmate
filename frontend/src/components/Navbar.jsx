30ebfa9a69a24fcd78a7343635d623c0f4971c56
Magne Svarverud Skaro <magneskaro@gmail.com>
2025-07-09 14:05:43 +0200

    changing searchbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -68,42 +68,54 @@ function Navbar({
             <div className="logo-narrow">BN</div> */}
           </Link>
         </h1>
-        <nav
-          className={`navLinksContainer ${isBurgerToggle && "navList-toggle"}`}
+        {!isSearch && (
+          <nav
+            className={`navLinksContainer ${
+              isBurgerToggle && "navList-toggle"
+            }`}
+          >
+            <ul>
+              <li>
+                <Link onClick={() => resetNavbar()} className="link" to="/">
+                  Hjem
+                </Link>
+              </li>
+              <li>
+                <Link onClick={() => resetNavbar()} className="link" to="/om">
+                  Om oss
+                </Link>
+              </li>
+              <li>
+                <Link
+                  onClick={() => resetNavbar()}
+                  className="link"
+                  to="/produkter"
+                >
+                  Produkter
+                </Link>
+              </li>
+              <li>
+                <Link
+                  onClick={() => resetNavbar()}
+                  className="link"
+                  to="/kontakt"
+                >
+                  Kontakt
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        )}
+        <div
+          className={`searchLoginCart ${
+            isSearch ? "searchLoginCart-active" : "searchLoginCart-inactive"
+          }`}
         >
-          <ul>
-            <li>
-              <Link onClick={() => resetNavbar()} className="link" to="/">
-                Hjem
-              </Link>
-            </li>
-            <li>
-              <Link onClick={() => resetNavbar()} className="link" to="/om">
-                Om oss
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={() => resetNavbar()}
-                className="link"
-                to="/produkter"
-              >
-                Produkter
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={() => resetNavbar()}
-                className="link"
-                to="/kontakt"
-              >
-                Kontakt
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="searchLoginCart">
-          <div className="searchContainer">
+          <div
+            className={`searchContainer ${
+              isSearch ? "searchContainer-active" : "searchContainer-inactive"
+            }`}
+          >
             {/* {isSearch ? (
               <div className="searchInputSection">
                 <button onClick={() => searchToggle()}>
@@ -183,108 +195,102 @@ function Navbar({
               </div>
             )}
           </div>
-          {!isSearch && (
-            <>
-              <div className="loginContainer">
-                <Link
-                  onClick={() => {
-                    handleLogout()
-                    resetNavbar()
-                  }}
-                  className="link"
-                  to={`${isLoggedIn ? "/" : "/innlogging"}`}
-                >
-                  {isLoggedIn ? (
-                    <button className="login-wide" onClick={() => handleLogout}>
-                      Logg ut
+          {/* {!isSearch && (
+            <> */}
+          <div className="loginContainer">
+            <Link
+              onClick={() => {
+                handleLogout()
+                resetNavbar()
+              }}
+              className="link"
+              to={`${isLoggedIn ? "/" : "/innlogging"}`}
+            >
+              {isLoggedIn ? (
+                <button className="login-wide" onClick={() => handleLogout}>
+                  Logg ut
+                </button>
+              ) : (
+                <button className="login-wide">Logg inn</button>
+              )}
+              <div className="login-narrow">
+                <i className="fa fa-user"></i>
+              </div>
+            </Link>
+          </div>
+          <div className="cartSection">
+            <button
+              className="cartText-wide pointer"
+              onClick={() => cartToggle()}
+              tabIndex={0}
+              aria-label="Forhåndsvis handlevogn"
+            >
+              Handlevogn
+            </button>
+            <button
+              className="cartIconContainer"
+              onClick={() => cartToggle()}
+              aria-label="Forhåndsvis handlevogn"
+            >
+              <i className="fa fa-shopping-cart"></i>
+              {cart.length > 0 && (
+                <div className="cartIconCircle">{cart.length}</div>
+              )}
+            </button>
+            {isCart && (
+              <div className="cartPreview">
+                <h5>Varer i handlevogn:</h5>
+
+                {cart.map((item) => (
+                  <div
+                    className="previewProductCard"
+                    key={item._id + " preview"}
+                  >
+                    <div className="cardImage">
+                      <img src={item.images[0]} />
+                    </div>
+                    <div className="cardText">{item.name}</div>
+                    <div className="cardAmount"></div>
+                    <button
+                      className="cartRemove"
+                      onClick={() =>
+                        setCart(
+                          cart.filter((product) => product._id !== item._id)
+                        )
+                      }
+                      aria-label={`Fjern ${item.name} (id: '${item._id}') fra handlevogn`}
+                    >
+                      &#10006;
                     </button>
-                  ) : (
-                    <button className="login-wide">Logg inn</button>
-                  )}
-                  <div className="login-narrow">
-                    <i className="fa fa-user"></i>
                   </div>
+                ))}
+
+                <Link
+                  onClick={() => resetNavbar()}
+                  className="toCartLink link"
+                  to="/handlevogn"
+                >
+                  <button
+                    className="toCartButton"
+                    aria-label="Gå til handlevogn"
+                  >
+                    Gå til handlevogn
+                  </button>
                 </Link>
               </div>
-              <div className="cartSection">
-                <button
-                  className="cartText-wide pointer"
-                  onClick={() => cartToggle()}
-                  tabIndex={0}
-                  aria-label="Forhåndsvis handlevogn"
-                >
-                  Handlevogn
-                </button>
-                <button
-                  className="cartIconContainer"
-                  onClick={() => cartToggle()}
-                  aria-label="Forhåndsvis handlevogn"
-                >
-                  <i className="fa fa-shopping-cart"></i>
-                  {cart.length > 0 && (
-                    <div className="cartIconCircle">{cart.length}</div>
-                  )}
-                </button>
-                {isCart && (
-                  <div className="cartPreview">
-                    <h5>Varer i handlevogn:</h5>
-
-                    {cart.map((item) => (
-                      <div
-                        className="previewProductCard"
-                        key={item._id + " preview"}
-                      >
-                        <div className="cardImage">
-                          <img src={item.images[0]} />
-                        </div>
-                        <div className="cardText">{item.name}</div>
-                        <div className="cardAmount"></div>
-                        <button
-                          className="cartRemove"
-                          onClick={() =>
-                            setCart(
-                              cart.filter((product) => product._id !== item._id)
-                            )
-                          }
-                          aria-label={`Fjern ${item.name} (id: '${item._id}') fra handlevogn`}
-                        >
-                          &#10006;
-                        </button>
-                      </div>
-                    ))}
-
-                    <Link
-                      onClick={() => resetNavbar()}
-                      className="toCartLink link"
-                      to="/handlevogn"
-                    >
-                      <button
-                        className="toCartButton"
-                        aria-label="Gå til handlevogn"
-                      >
-                        Gå til handlevogn
-                      </button>
-                    </Link>
-                  </div>
-                )}
-              </div>
-              <button
-                className="burger"
-                onClick={() => toggleBurger()}
-                aria-label={`${isBurgerToggle ? "Lukk" : "Åpne"} sidemeny`}
-              >
-                <div
-                  className={`line1 ${isBurgerToggle && "line1-toggle"}`}
-                ></div>
-                <div
-                  className={`line2 ${isBurgerToggle && "line2-toggle"}`}
-                ></div>
-                <div
-                  className={`line3 ${isBurgerToggle && "line3-toggle"}`}
-                ></div>
-              </button>
-            </>
-          )}
+            )}
+          </div>
+          <button
+            className="burger"
+            onClick={() => toggleBurger()}
+            aria-label={`${isBurgerToggle ? "Lukk" : "Åpne"} sidemeny`}
+          >
+            <div className={`line1 ${isBurgerToggle && "line1-toggle"}`}></div>
+            <div className={`line2 ${isBurgerToggle && "line2-toggle"}`}></div>
+            <div className={`line3 ${isBurgerToggle && "line3-toggle"}`}></div>
+          </button>
+          {/* </>
+          )} */}
         </div>
       </div>
       {isBurgerToggle && <div className="darkScreen"></div>}
